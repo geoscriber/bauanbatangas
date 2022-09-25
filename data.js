fc0a038fe8433c618cf08db3037d5489c0a9f31f
krquinton <nscrexgst.dotr@gmail.com>
2022-09-25 18:52:18 +0800

    commit

---
+++ data.js
@@ -0,0 +1,173 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-top-view",
+      "name": "Top View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.006412769349523728,
+        "pitch": 0.78699060757776,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-rear",
+      "name": "Rear",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4833285774383995,
+        "pitch": 0.39046129537170593,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-north",
+      "name": "North",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2797332638193719,
+        "pitch": 0.1033258874319003,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-center",
+      "name": "Center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.12217171518567937,
+        "pitch": 0.08407061383601899,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7097011009792187,
+          "pitch": -0.5511429936877512,
+          "rotation": 0,
+          "target": "0-top-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-south",
+      "name": "South",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3665191429188148,
+        "pitch": 0.5679437023588516,
+        "fov": 1.35820747241737
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Bauan, Batangas",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
